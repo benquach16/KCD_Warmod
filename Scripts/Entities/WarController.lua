317aa43bddb0ce1694dcb790725975c6df60e1f3
Ben Quach <benquach16@gmail.com>
2020-09-12 22:26:28 -0700

    add commanders

--- Scripts/Entities/WarController.lua
+++ Scripts/Entities/WarController.lua
@@ -5,13 +5,13 @@ WarConstants = {
 	troopCost = 100,
 	cumanSoldier = "4957c994-1489-f528-130c-a00b9838a4a5",
 	ratSoldier = "43b48356-ecf4-5e6e-bce4-1d98ed745baa",
-	numWaves = 5,
+	numWaves = 4,
 	corpseTime = 5000, --5 seconds
-	victoryTime = 30000, -- 30 seconds
-	waveInterval = 40000 -- 40 seconds
+	victoryTime = 20000, -- 30 seconds
+	waveInterval = 65000 -- 50 seconds
 }
 
-WarTypes = {
+WarTroopTypes = {
 	commander = 0,
 	knight = 1,
 	halberd = 2,
@@ -21,25 +21,31 @@ WarTypes = {
 	aux_light = 6
 }
 
+BattleTypes = {
+	Attack,
+	Defend,
+	Field
+}
+
 WarGuids = {
 	knight = {},
 	halberd = {},
 	aux = {}
 }
 
-WarGuids[WarTypes.knight] = {}
-WarGuids[WarTypes.knight][WarConstants.rat_side] = "41429725-5368-3cb1-6440-2e2e02b4fc97"
-WarGuids[WarTypes.knight][WarConstants.cuman_side] = "49c00005-e5e9-ee50-7370-8bc12c8ad29f"
-WarGuids[WarTypes.halberd] = {}
-WarGuids[WarTypes.halberd][WarConstants.rat_side] = "43b48356-ecf4-5e6e-bce4-1d98ed745baa"
-WarGuids[WarTypes.halberd][WarConstants.cuman_side] = "4957c994-1489-f528-130c-a00b9838a4a5"
-WarGuids[WarTypes.aux] = {}
-WarGuids[WarTypes.aux][WarConstants.rat_side] = "4aa17e70-525a-1e83-d32f-adf2f8c60daf"
-WarGuids[WarTypes.aux][WarConstants.cuman_side] = "4c4f6e9d-aa80-4f1b-a9d9-62573e6de2a7"
+WarGuids[WarTroopTypes.knight] = {}
+WarGuids[WarTroopTypes.knight][WarConstants.rat_side] = "41429725-5368-3cb1-6440-2e2e02b4fc97"
+WarGuids[WarTroopTypes.knight][WarConstants.cuman_side] = "49c00005-e5e9-ee50-7370-8bc12c8ad29f"
+WarGuids[WarTroopTypes.halberd] = {}
+WarGuids[WarTroopTypes.halberd][WarConstants.rat_side] = "43b48356-ecf4-5e6e-bce4-1d98ed745baa"
+WarGuids[WarTroopTypes.halberd][WarConstants.cuman_side] = "4957c994-1489-f528-130c-a00b9838a4a5"
+WarGuids[WarTroopTypes.aux] = {}
+WarGuids[WarTroopTypes.aux][WarConstants.rat_side] = "4aa17e70-525a-1e83-d32f-adf2f8c60daf"
+WarGuids[WarTroopTypes.aux][WarConstants.cuman_side] = "4c4f6e9d-aa80-4f1b-a9d9-62573e6de2a7"
 
 WarLocations = {
-	--{center = { x=3087.410, y=829.085, z=118.978 }, rat = {x = 3040.93,y = 850.747,z = 119.558}, cuman = {x = 3136.570,y= 854.815,z= 122.557}},
-	{center = { x=3106.441, y=819.985, z=118.436 }, rat = {x = 3046.669,y = 798.363,z = 115.952}, cuman = {x = 3136.570,y= 854.815,z= 122.557}, name="Rattay Farmhouse"}
+	--{center = {x = 3136.570,y= 854.815,z= 122.557}, rat = {x = 3046.669,y = 798.363,z = 115.952}, cuman = {x = 3192.020,y= 910.343,z= 127.216}, name="Rattay Farmhouse"}
+	{center = {x = 3136.570,y= 854.815,z= 122.557}, rat = {x = 2995.868,y = 809.014,z = 113.108}, cuman = {x = 3136.570,y= 854.815,z= 122.557}, name="Rattay Farmhouse"}
 }
 WarLocationstest = {
 	{center = { x=47.969, y=43.522, z=33.583}, rat = { x=27.969, y=43.522, z=33.583}, cuman = { x=67.969, y=43.522, z=33.583}, name="Test battleground"}
@@ -57,8 +63,8 @@ Battle = {
 	cuman_point = nil,
 	locations = nil,
 	wavesleft = WarConstants.numWaves,
-	rattayStrengthPerWave = 3, -- temp
-	cumanStrengthPerWave = 3, -- temp
+	rattayStrengthPerWave = 6, -- temp
+	cumanStrengthPerWave = 6, -- temp
 	rattayTroops = {},
 	cumanTroops = {},
 	
@@ -74,7 +80,8 @@ WarController = {
 	-- only 1 battle can occur at a time
 	inBattle = false,
 	readyForNewBattle = true,
-	currentBattle = Battle
+	currentBattle = Battle,
+	nextBattleLocation = nil
 }
 
 function WarController:OnSpawn()
@@ -112,8 +119,10 @@ function getrandomposnear(position, radius)
 	ret.x = position.x
 	ret.y = position.y
 	ret.z = position.z
-	ret.x = position.x + math.random() + math.random(0, radius) - math.random(0, radius)
-	ret.y = position.y + math.random() + math.random(0, radius) - math.random(0, radius)
+	if radius > 0 then
+		ret.x = position.x + math.random() - math.random() + math.random(0, radius) - math.random(0, radius)
+		ret.y = position.y + math.random() - math.random() + math.random(0, radius) - math.random(0, radius)
+	end
 	return ret
 end
 
@@ -149,7 +158,7 @@ function WarController:Spawn(side, position, objective, troopType)
 	spawnParams.properties.sharedSoulGuid = WarGuids[troopType][side]
 	if side == WarConstants.cuman_side then
 		self.currentBattle.numCuman = self.currentBattle.numCuman + 1
-		--isEnemy = true
+		isEnemy = true
 	else
 		self.currentBattle.numRattay = self.currentBattle.numRattay + 1
 	end
@@ -163,13 +172,79 @@ function WarController:Spawn(side, position, objective, troopType)
 	
 	local initmsg = Utils.makeTable('skirmish:init',{controller=player.this.id,isEnemy=isEnemy,oponentsNode=player.this.id,useQuickTargeting=true,targetingDistance=5.0, useMassBrain=true})
 	XGenAIModule.SendMessageToEntityData(entity.this.id,'skirmish:init',initmsg);
-	local initmsg2 = Utils.makeTable('skirmish:command',{type="attackMove",target=objective.this.id, randomRadius=0.5})
-	XGenAIModule.SendMessageToEntityData(entity.this.id,'skirmish:command',initmsg2);
+	local initmsg2 = Utils.makeTable('skirmish:soundSetup',{ intensity=3.0, intensityPerEnemy=0.0, trigger="test"})
+	XGenAIModule.SendMessageToEntityData(entity.this.id,'skirmish:soundSetup',initmsg2);
+	local initmsg3 = Utils.makeTable('skirmish:barkSetup',{ topicLabel="prib_ramGate", cooldown="5s", once=false, command="*", forceSubtitles = true})
+	XGenAIModule.SendMessageToEntityData(entity.this.id,'skirmish:barkSetup',initmsg3);
+	local initmsg4 = Utils.makeTable('skirmish:command',{type="attackMove",target=objective.this.id, randomRadius=0.5, movementSpeed="AlertedWalk"})
+	XGenAIModule.SendMessageToEntityData(entity.this.id,'skirmish:command',initmsg4);
 	
 	table.insert(self.currentBattle.troops, entity)
 end
 
+-- Very specific helper function, don't use all willy nilly
+function WarController:SpawnOnlyEntity(side, position, troopType, radius)
+	--System.LogAlways("$5 Attempting to Spawn troop")
+	local spawnParams = {}
+	local isEnemy = false
+	spawnParams.class = "NPC"
+	spawnParams.name = "soldier"
+	spawnParams.position=getrandomposnear(position, radius)
+	spawnParams.properties = {}
+	spawnParams.properties.sharedSoulGuid = WarGuids[troopType][side]
+	if side == WarConstants.cuman_side then
+		self.currentBattle.numCuman = self.currentBattle.numCuman + 1
+		isEnemy = true
+	else
+		self.currentBattle.numRattay = self.currentBattle.numRattay + 1
+	end
+	spawnParams.properties.warmodside = side
+	spawnParams.properties.bWH_PerceptibleObject = 1
+	local entity = System.SpawnEntity(spawnParams)
+	
+	entity.soul:AddPerk(string.upper("d2da2217-d46d-4cdb-accb-4ff860a3d83e")) -- perfect block
+	entity.soul:AddPerk(string.upper("ec4c5274-50e3-4bbf-9220-823b080647c4")) -- riposte
+	entity.soul:AddPerk(string.upper("3e87c467-681d-48b5-9a8c-485443adcd42")) -- pommel strike
+	
+	local initmsg = Utils.makeTable('skirmish:init',{controller=player.this.id,isEnemy=isEnemy,oponentsNode=player.this.id,useQuickTargeting=true,targetingDistance=5.0, useMassBrain=true})
+	XGenAIModule.SendMessageToEntityData(entity.this.id,'skirmish:init',initmsg);
+	
+	return entity
+end
+
+function WarController:SpawnSquad(side, position, objective, strength)
+	local num = 0
+	for i=0,strength,1 do
+		self:Spawn(side, position, objective, WarTroopTypes.halberd)
+	end
+	self:Spawn(side, position,objective, WarTroopTypes.knight)
+	--self:Spawn(side, position,objective, WarTroopTypes.knight)
+	self:Spawn(side, position,objective, WarTroopTypes.aux)
+end
+
+function WarController:SpawnLeader(side, position)
+	System.LogAlways("spawning leaders")
+	local leader = self:SpawnOnlyEntity(side, position, WarTroopTypes.knight, 0)
+	if side == WarConstants.cuman_side then
+		leader.actor:EquipClothingPreset("47875046-71f3-a06f-8ec9-ea8546176b8d")
+	else
+		leader.actor:EquipClothingPreset("4a45929c-690e-b01a-95ef-f975bbf6b0bd")
+	end
+	leader.soul:AdvanceToStatLevel("vit", 25)
+	leader.soul:AdvanceToStatLevel("str", 20)
+	leader.soul:SetState("health", 500)
+	leader.soul:OverrideHead("404f142d-dc33-769c-f121-ac7a579c7fbc")
+	local bodyguard = self:SpawnOnlyEntity(side, position, WarTroopTypes.knight, 2.0)
+	table.insert(self.currentBattle.troops, bodyguard)
+	local bodyguard2 = self:SpawnOnlyEntity(side, position, WarTroopTypes.knight, 2.0)
+	table.insert(self.currentBattle.troops, bodyguard2)
+end
+
 function WarController.SpawnWave(self)
+	if self.inBattle == false then
+		System.LogAlways("Somehow tried to spawn after battle was finished")
+		return
+	end
 	--System.LogAlways("spawning new wave")
 	message = "Troops have arrived! " .. tostring(self.currentBattle.wavesleft) .. " waves left\n"
 	Game.SendInfoText(message,false,nil,5)
@@ -177,16 +252,9 @@ function WarController.SpawnWave(self)
 		Script.SetTimer(WarConstants.waveInterval, self.SpawnWave, self)
 		self.currentBattle.wavesleft = self.currentBattle.wavesleft - 1
 	end
-	for i=0,self.currentBattle.cumanStrengthPerWave,1 do
-		self:Spawn(WarConstants.cuman_side, self.currentBattle.locations.cuman, self.currentBattle.rat_point, WarTypes.halberd)
-	end
-	self:Spawn(WarConstants.cuman_side, self.currentBattle.locations.cuman, self.currentBattle.rat_point, WarTypes.knight)	
-	self:Spawn(WarConstants.cuman_side, self.currentBattle.locations.cuman, self.currentBattle.rat_point, WarTypes.aux)
-	for i=0,self.currentBattle.rattayStrengthPerWave,1 do
-		self:Spawn(WarConstants.rat_side, self.currentBattle.locations.rat, self.currentBattle.cuman_point, WarTypes.halberd)
-	end
-	self:Spawn(WarConstants.rat_side, self.currentBattle.locations.rat, self.currentBattle.cuman_point, WarTypes.knight)
-	self:Spawn(WarConstants.rat_side, self.currentBattle.locations.rat, self.currentBattle.cuman_point, WarTypes.aux)
+	
+	self:SpawnSquad(WarConstants.cuman_side, self.currentBattle.locations.cuman, self.currentBattle.rat_point, self.currentBattle.cumanStrengthPerWave)
+	self:SpawnSquad(WarConstants.rat_side, self.currentBattle.locations.rat, self.currentBattle.cuman_point, self.currentBattle.rattayStrengthPerWave)
 end
 
 function WarController.RemoveCorpse(entity)
@@ -219,11 +287,13 @@ function WarController:GenerateBattle()
 	self.currentBattle.cuman_point = self:CreateAITagPoint(locations.cuman)
 	self.currentBattle.locations = locations
 	--Dump(center)
-	self.SpawnWave(self)
-	
 	self.inBattle = true
 	self.readyForNewBattle = false
 	Utils.DisableSave(WarConstants.cSaveLockName, enum_disableSaveReason.battle)
+	
+	self.SpawnWave(self)
+	self:SpawnLeader(WarConstants.cuman_side, self.currentBattle.locations.cuman)
+	self:SpawnLeader(WarConstants.rat_side, self.currentBattle.locations.rat)
 end
 
 function WarController:DetermineVictor()
@@ -250,10 +320,10 @@ function WarController:CheckDeaths()
 	--System.LogAlways("checking died")
 	for key,value in pairs(self.currentBattle.troops) do
 		if value.soul:GetState("health") < 1 then
-			System.LogAlways("someone died")
-			Script.SetTimer(WarConstants.waveInterval, self.RemoveCorpse, value)
+			--System.LogAlways("someone died")
+			Script.SetTimer(WarConstants.corpseTime, self.RemoveCorpse, value)
 			self.currentBattle.troops[key] = nil
-			Dump(value.Properties)
+			--Dump(value.Properties)
 			if value.Properties.warmodside == WarConstants.cuman_side then
 				self.currentBattle.numCuman = self.currentBattle.numCuman - 1
 			else
@@ -280,6 +350,7 @@ function WarController:OnUpdate(delta)
 		end
 	else
 		--generate battles and do general war AI things
+		local position = player:GetWorldPos()
 	end
 end
 
